14fde0efb84451e80883fd04663032387c9372e0
annaspri <37249585+annaspri@users.noreply.github.com>
2020-10-02 17:25:52 -0400

    Modified custom home page appearance based on user research group

--- public2/module_js/mods.js
+++ public2/module_js/mods.js
@@ -1,32 +1,46 @@
 {
+  async function getGroupData(){
+    const groupData = await $.getJSON('/json/groupData.json', function(data) {
+      return data;
+    });
+    return groupData;
+  }
+
+  // returns array with ["month","date","year"]
+  function parseDateInformation(dateString){
+    const dateInfo = dateString.split("/");
+    return dateInfo;
+  }
+
+  function showModulesInPhase(groupData){
+    let currentPhase = "phase1";
+    // show the appropriate modules
+    let modName = groupData.groups[userGroup][currentPhase];
+    $(`a.ui.card[data-modCard=${modName}]`).show();
+    for (phaseInfo in groupData.startTimes){
+      const dateInfo = parseDateInformation(groupData.startTimes[phaseInfo]);
+      const month = dateInfo[0] - 1;
+      const day = dateInfo[1];
+      const year = dateInfo[2];
+      const phaseStart = new Date(year, month, day, 0, 1, 0).getTime();
+      const currentTime = new Date().getTime();
+      if (currentTime >= phaseStart) {
+        currentPhase = phaseInfo;
+        // show the appropriate modules
+        modName = groupData.groups[userGroup][currentPhase];
+        $(`a.ui.card[data-modCard=${modName}]`).show();
+      }
+    }
+  }
+
   function showModuleCards(cardList){
     for (const modName in cardList) {
       $(`a.ui.card[data-modCard=${cardList[modName]}]`).css("display","block");
     }
   }
 
-  $(window).on("load", function(){
-    const groupAssignments = {
-      "A": {
-        "phase1": ["esteem","targeted"],
-        "phase2": ["cyberbullying","phishing"]
-      },
-      "B": {
-        "phase1": ["digital-literacy","digfoot"],
-        "phase2": ["esteem"]
-      }
-    }
-    const phase2Start = new Date(2020, 8, 29, 20, 0, 0).getTime();
-    const finalPhaseStart = new Date(2020, 8, 29, 20, 23, 0).getTime();
-    const cardList1 = groupAssignments[userGroup]["phase1"];
-    showModuleCards(cardList1);
-    const currentTime = new Date().getTime();
-    if(currentTime >= phase2Start){
-      const cardList2 = groupAssignments[userGroup]["phase2"];
-      showModuleCards(cardList2);
-    }
-    if(currentTime >= finalPhaseStart){
-      showModuleCards(["eval"]);
-    }
+  $(window).on("load", async function(){
+    const groupData = await getGroupData();
+    const currentPhase = showModulesInPhase(groupData);
   })
 }
